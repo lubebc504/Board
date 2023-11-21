4481f42fa710b6b602dd419bef8990dae6fc09ab
lubebc504 <jack209014@gmail.com>
2023-11-21 10:18:45 +0900

    Practice Private Api
    whsskdjfudnj

--- practice/src/main/java/regist/practice/Api.java
+++ practice/src/main/java/regist/practice/Api.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 
 public class Api {
     public static void main(String[] args) throws IOException, JSONException {
+
+
+        PrivateApi();
+    }
+
+    public static void PublicApi() throws IOException, JSONException {
         EntityManager EM;
         OkHttpClient client =new OkHttpClient();
         Request requset = new Request.Builder()
@@ -26,6 +32,24 @@ public class Api {
         String temp=json.getString("data");
         JSONObject json2 = new JSONObject(temp);
         System.out.println("hi"+json2.getString("opening_price"));
+    }
 
+    public static void PrivateApi() throws IOException {
+        OkHttpClient client1 = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body1 = RequestBody.create(mediaType, "order_currency=string&payment_currency=string");
+        Request request = new Request.Builder()
+                .url("https://api.bithumb.com/info/account")
+                .post(body1)
+                .addHeader("accept", "application/json")
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("Api-Key", "2e969fdde4db75b5abbe099890df4062")
+                .addHeader("Api-Nonce", "System.currentTimeMillis()")
+                .addHeader("Api-Sign", "상세 가이드 참고")
+                .build();
+
+        Response response1 = client1.newCall(request).execute();
     }
 }
+
