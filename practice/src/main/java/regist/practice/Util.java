1fcc50aad5d5b4c2889354213c77bb542726aa58
lubebc504 <85859875+lubebc504@users.noreply.github.com>
2023-11-21 19:10:06 +0900

    UptoCom

--- practice/src/main/java/regist/practice/Util.java
+++ practice/src/main/java/regist/practice/Util.java
@@ -10,8 +10,9 @@ import java.util.Map.Entry;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import com.google.common.io.BaseEncoding;
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+//import com.google.common.io.BaseEncoding;
+//import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import org.apache.xerces.impl.dv.util.Base64;
 
 public class Util {
 
